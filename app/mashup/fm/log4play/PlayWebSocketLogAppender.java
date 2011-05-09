830908519224b47571ffd1b9cc2446a1a0239574
Felipe Oliveira <felipera@felipera-macbook.local>
2011-05-09 19:11:18 -0400

    first commit

---
+++ app/mashup/fm/log4play/PlayWebSocketLogAppender.java
@@ -0,0 +1,8 @@
+package mashup.fm.log4play;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.WriterAppender;
+
+public class PlayWebSocketLogAppender extends WriterAppender implements Appender {
+
+}
